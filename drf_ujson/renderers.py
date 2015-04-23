dc2c58d54bd4d71b9b69bae13de8f49d5ccbecb6
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-04-23 15:11:15 +0200

    Added support for indent and disabled escape slashes.

--- drf_ujson/renderers.py
+++ drf_ujson/renderers.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
+from django.http.multipartparser import parse_header
 from rest_framework.compat import six
 from rest_framework.renderers import BaseRenderer
+from rest_framework.settings import api_settings
 import ujson
 
 
@@ -13,17 +15,49 @@ class UJSONRenderer(BaseRenderer):
 
     media_type = 'application/json'
     format = 'json'
-    ensure_ascii = True
+    ensure_ascii = not api_settings.UNICODE_JSON
+    compact = api_settings.COMPACT_JSON
     charset = None
 
-    def render(self, data, *args, **kwargs):
+    def get_indent(self, accepted_media_type, renderer_context):
+        if accepted_media_type:
+            # If the media type looks like 'application/json; indent=4',
+            # then pretty print the result.
+            # Note that we coerce `indent=0` into `indent=None`.
+            base_media_type, params = parse_header(accepted_media_type.encode('ascii'))
+            try:
+                return max(min(int(params['indent']), 8), 0)
+            except (KeyError, ValueError, TypeError):
+                pass
 
+        # If 'indent' is provided in the context, then pretty print the result.
+        # E.g. If we're being called by the BrowsableAPIRenderer.
+        return renderer_context.get('indent', 0)
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        """
+        Render `data` into JSON, returning a bytestring.
+        """
         if data is None:
             return bytes()
 
-        ret = ujson.dumps(data, ensure_ascii=self.ensure_ascii)
+        renderer_context = renderer_context or {}
+        indent = self.get_indent(accepted_media_type, renderer_context)
 
-        # force return value to unicode
+        ret = ujson.dumps(
+            data,
+            indent=indent, ensure_ascii=self.ensure_ascii, escape_forward_slashes=not indent
+        )
+
+        # On python 2.x json.dumps() returns bytestrings if ensure_ascii=True,
+        # but if ensure_ascii=False, the return type is underspecified,
+        # and may (or may not) be unicode.
+        # On python 3.x json.dumps() returns unicode strings.
         if isinstance(ret, six.text_type):
+            # We always fully escape \u2028 and \u2029 to ensure we output JSON
+            # that is a strict javascript subset. If bytes were returned
+            # by json.dumps() then we don't have these characters in any case.
+            # See: http://timelessrepo.com/json-isnt-a-javascript-subset
+            ret = ret.replace('\u2028', '\\u2028').replace('\u2029', '\\u2029')
             return bytes(ret.encode('utf-8'))
         return ret
